ccf84b89e48c08a5d89786849c66995c0fd103e5
pichaachuu <nafisahaliyah01@gmail.com>
2024-11-08 13:09:41 +0700

    Rata Nilai

---
+++ RataNilai13.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class RataNilai13 {
+    public static void main(String[] args) {
+        Scanner input13 = new Scanner(System.in);
+
+        int i , j;
+        float nilai, totalNilai, rataNilai;
+
+        System.out.print("Masukkan jumlah mahasiswa = ");
+        int jmlMhs = input13.nextInt();
+
+        i = 1;
+        while (i<=jmlMhs) {
+            System.out.println("Input Nilai Mahasiswa ke-" + i);
+            totalNilai = 0;
+            for (j=1;j<=5;j++) {
+                System.out.print("Nilai ke-" + j + " = " );
+                nilai = input13.nextInt();
+                totalNilai += nilai;
+            }
+            rataNilai = totalNilai/5;
+            System.out.println("Rata-rata Nilai Mahasiswa ke-" + i + " adalah " + rataNilai);
+            i++;
+        }
+
+    }
+}
