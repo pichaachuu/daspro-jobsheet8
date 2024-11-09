b76d0d6adf78911fd32c422eedf43d43a702ceaa
pichaachuu <nafisahaliyah01@gmail.com>
2024-11-09 17:44:35 +0700

    Pertanyaan !

---
+++ Pertanyaan1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Pertanyaan1 {
+
+    public static void main(String[] args) {
+        Scanner input13 = new Scanner(System.in);
+
+        int N;
+
+        System.out.print("Masukkan jumlah N = ");
+        N = input13.nextInt();
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j<=N;j++) {
+                if (i == 1 || i == N || j == 1 || j == N) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
